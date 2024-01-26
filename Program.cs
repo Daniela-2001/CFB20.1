e5572382f2d3086d8a5c06a86221c26834b3cc30
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-26 20:08:19 +0000

    CFB20.1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace aula20
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] num = new int[10];
+
+            int i = 0;
+            while (i < 10)
+            { //10
+                Console.WriteLine("CFB Cursos");
+                i++;
+            }
+            Console.WriteLine("Fim do loop");
+        }
+    }
+}
